e283b99695a15b34d3ee6a900086928cc7220d37
DESKTOP-1L38NVA\Nilesh Devdas <nilesh.devdas@gmail.com>
2018-08-29 11:57:58 +0530

    Initial Commit

---
+++ src/test/java/siemens/e2e/E2eApplicationTests.java
@@ -0,0 +1,16 @@
+package siemens.e2e;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class E2eApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
